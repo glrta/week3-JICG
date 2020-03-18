c795368b8e037a45c2402805901d946bf61e268e
Chloe <chloeh24@gmail.com>
2020-03-18 16:30:27 +0000

    add items with template format

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const list = document.querySelector('.list')
-const listItems = document.getElementsByTagName('li')
+const listItems = document.getElementsByTagName('label')
 const addButton = document.querySelector('.add')
 let inputBox = document.querySelector('#todo-item')
 
@@ -10,12 +10,33 @@ console.log(listItems)
 addButton.addEventListener('click', (event) => {
     event.preventDefault()
     let userInput = inputBox.value
-    const node = document.createElement('li');      // Create a <li> node
-    const newItem = document.createTextNode(userInput);    // Create a text node
-    node.appendChild(newItem);   // Append the text to <li>
-    list.appendChild(node);    // Append <li> to <ul> 
-    userInput = ''
+    const template = document.querySelector("#todoTemplate");
+    const frag = template.content.cloneNode(true);
+    frag.querySelector('.todo__text').textContent = userInput
+    list.appendChild(frag)
+
 })
+
+
+// / this function uses the <template> defined in the HTML
+// // it'll only get longer if we need to add more dynamic properties
+// function createCardUsingTemplate(article) {
+//   const { title, summary, image } = article;
+//   // get the template for articles
+//   const template = document.querySelector("#articleTemplate");
+//   // clone the content of the template (returns a DocumentFragment)
+//   const domFragment = template.content.cloneNode(true);
+//   // we only need to add the dynamic properties inside the fragment
+//   // since all the static ones (e.g. styling classNames) are set in the template
+//   domFragment.querySelector("h3").textContent = title;
+//   domFragment.querySelector("img").src = image;
+//   domFragment.querySelector("p").textContent = summary;
+//   // put the fragment onto the page
+//   containerElement2.appendChild(domFragment);
+// }
+
+// articles.forEach(createCard);
+// articles.forEach(createCardUsingTemplate);
 
 
 test('Should add a list item', t => {  
@@ -28,23 +49,9 @@ test('Should add a list item', t => {
 test('Should add user input item', t => { 
     inputBox.value = 'Wash dishes'
     addButton.click()
-    t.equal(list[3], 'Wash dishes')
+    t.equal(listItems[1].textContent, 'Wash dishes')
 });
 
-
-
-// const firstNumber = document.querySelector("#a");
-// const secondNumber = document.querySelector("#b");
-// const operator = document.querySelector("#sign");
-// const submitButton = document.querySelector("button[type='submit']");
-// const result = document.querySelector("#result");
-
-// test("User input displays result correctly once form is submitted", obj => {
-//     firstNumber.value = "-2";
-//     secondNumber.value = "3";
-//     operator.value = "+";
-//     submitButton.click();
-//     obj.equal(result.textContent, "1");
+test('Should add items within template format', t => {
     
-//     result.textContent = "";
-// });
+})
