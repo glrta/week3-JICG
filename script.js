f2a655448e95ae768b7124024de644aaa6a5a588
Ivo Evans <ievans.storrie@gmail.com>
2020-03-19 11:28:45 +0000

    Add event listeners while copying template to DOM Relates #2
    Co-authored-by: Chloe <chloeh24@hotmail.com>

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-const todos = document.querySelectorAll('.todo') // these are not rendered until a function makes them
 const list = document.querySelector('.list')
 const listItems = document.getElementsByTagName('label')
 const addButton = document.querySelector('.add')
-let inputBox = document.querySelector('#todo-item');
+const inputBox = document.querySelector('#todo-item');
 
-todos.forEach(todo => {
-  todo.addEventListener('click', (event) => {
-    let text = todo.querySelector('label')
-    let e = event.target.tagName;
-
-    if (e === "INPUT") { // label elements cause a click event on their corresponding input
-      if (Array.from(text.classList).includes('todo__text--done')) {
-        text.classList.remove('todo__text--done')
-      } else {
-        text.classList.add('todo__text--done')
-      }
-    }
-
-  })
-})
   
 addButton.addEventListener('click', submitHandler); 
 
@@ -33,6 +17,18 @@ function submitHandler(event) {
     const template = document.querySelector("#todoTemplate");
     const domFrag = template.content.cloneNode(true);
     domFrag.querySelector('.todo__text').textContent = userInput
+    const todo = domFrag.querySelector('.todo')
+
+    todo.addEventListener('click', (event) => {
+      let text = todo.querySelector('label')
+      let e = event.target.tagName;
+  
+      if (e === "INPUT") { 
+          text.classList.toggle('todo__text--done')
+      }
+    })
+
     list.appendChild(domFrag)
+
     inputBox.value = ""; 
-} 
+}
