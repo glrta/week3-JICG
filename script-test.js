6ae00c04a80ddcbbbb194d9f10a22d7106eac7f4
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-03-19 12:37:46 +0000

    delete functionality
    Relates #4

--- script-test.js
+++ script-test.js
@@ -1,35 +1,55 @@
+console.groupCollapsed("TEST: Can add items")
+    
+    test('Should add a list item', t => {  
+        inputBox.value = 'Walk dog';
+        addButton.click();
+        t.equal(listItems.length, 1);
+    });
 
-test('Should add a list item', t => {  
-    inputBox.value = 'Walk dog'
-    addButton.click()
-    t.equal(listItems.length, 1)
-});
+    test('Should add user input item', t => { 
+        inputBox.value = 'Wash dishes'
+        addButton.click()
+        t.equal(listItems[listItems.length-1].textContent, 'Wash dishes')
+    });
 
-test('Should add user input item', t => { 
-    inputBox.value = 'Wash dishes'
-    addButton.click()
-    t.equal(listItems[listItems.length-1].textContent, 'Wash dishes')
-});
+    function isTemplate(item){
+        return document.body.contains(document.getElementById('todoTemplate'));
+    }
 
-function isTemplate(item){
-    return document.body.contains(document.getElementById('todoTemplate'));
-}
+    test('Should add items within template format', t => {
+        t.equal(isTemplate(list), true)
+    })
 
-test('Should add items within template format', t => {
-    t.equal(isTemplate(list), true)
-})
+    test('Does not add anything if input box is empty', t =>{
+        let lengthBefore = listItems.length;
+        inputBox.value = ""
+        addButton.click()
+        let lengthAfter = listItems.length
+        t.equal(lengthBefore, lengthAfter);
+    })
 
-test('Does not add anything if input box is empty', t =>{
-    let lengthBefore = listItems.length
-    inputBox.value = ""
-    addButton.click()
-    let lengthAfter = listItems.length
-    t.equal(lengthBefore, lengthAfter);
-})
+    test('Clears box after input added', t => {
+        inputBox.value = "something"
+        addButton.click()
+        t.equal(inputBox.value, "");
+    })
+        
+console.groupEnd("TEST: Can add items")
 
-test('Clears box after input added', t => {
-    inputBox.value = "something"
-    addButton.click()
-    t.equal(inputBox.value, "");
-})
+console.groupCollapsed("TEST: Can delete items")
 
+    test("Can we delete a list item by clicking its delete button", t => {
+        
+        t.equal(,);
+    })
+
+    // test("Can we select an item's node by clicking its delete button", t => {
+    //     let deleteButtons = list.getElementsByTagName('button');
+    //     deleteButtons.forEach(deleteButton => {
+    //         deleteButton.click();
+    //     })
+    //     t.equal(focusElement,1);
+    // })
+
+console.groupEnd("TEST: Can delete items")
+    
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let inputBox = document.querySelector('#todo-item')
 
 addButton.addEventListener('click', submitHandler); 
 
-// Trying to fic console problem
+// Trying to fix console problem
 // addButton.addEventListener('keydown', function (e) {
 //     if (e.keyCode === 13) {        
 //         submitHandler;           
@@ -17,10 +17,10 @@ function submitHandler(event) {
     event.preventDefault()
 
     let userInput = inputBox.value
-    if(!userInput){
-        alert('write something!');
-        return;
-    }
+    // if(!userInput){
+    //     alert('write something!');
+    //     return;
+    // }
     const template = document.querySelector("#todoTemplate");
     const domFrag = template.content.cloneNode(true);
     domFrag.querySelector('.todo__text').textContent = userInput
